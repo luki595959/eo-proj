2f5790f8786fcdc1d128798411940eba9527e39a
luki595959 <57019378+luki595959@users.noreply.github.com>
2021-05-27 00:23:51 +0200

    Add files via upload

---
+++ app/routes/auctions.ts
@@ -0,0 +1,79 @@
+import { Router, Request, Response } from 'express'
+import { couldStartTrivia } from 'typescript';
+import { database } from '../database'
+
+function removeDuplicates(arr, key) {
+  if (!(arr instanceof Array) || key && typeof key !== 'string') {
+    return false;
+  }
+  if (key && typeof key === 'string') {
+    return arr.filter((obj, index, arr) => {
+      return arr.map(mapObj => mapObj[key]).indexOf(obj[key]) === index;
+    });
+  } else {
+    return arr.filter(function (item, index, arr) {
+      return arr.indexOf(item) == index;
+    });
+  }
+}
+
+
+const router = Router()
+router.get('/', async (req: Request, res: Response) => {
+  const db = await database()
+  const row = await db.all(
+    'SELECT country, count(country) FROM auctions group by country'
+  )
+
+  return res.render("index", { data: row })
+})
+
+router.get('/auctions/new', async (req: Request, res: Response) => {
+  const db = await database()
+  const row = await db.all(
+    'SELECT * FROM auctions ORDER BY auctionStart DESC LIMIT 15'
+  )
+  return res.render("auctions/auc_new", { data: row })
+})
+
+
+
+router.get('/auctions/q/:name', async (req: Request, res: Response) => {
+  const { name } = req.params
+  const db = await database()
+  const row = await db.all(
+    `SELECT * FROM auctions WHERE auctionName LIKE "%${name}%" LIMIT 5`
+  )
+  const ll = await db.all(
+    'SELECT * FROM auctions LIMIT 5000'
+  )
+  const offers = await db.all(
+    'SELECT * FROM offers WHERE auctionUniqueId = ?',
+    [row[0].auctionUniqueId]
+  )
+  var t = removeDuplicates(ll, "auctionName")
+  console.log(row)
+  console.log(offers)
+  return res.render("auctions/auc_name", { data: row, offers: offers, ll: t })
+})
+
+router.get('/auctions/:auctionId', async (req: Request, res: Response) => {
+  const { auctionId } = req.params
+  const db = await database()
+  const row = await db.get('SELECT * FROM auctions WHERE auctionUniqueId = ?', [
+    auctionId,
+  ])
+  const offers = await db.all(
+    'SELECT * FROM offers WHERE auctionUniqueId = ?',
+    [auctionId]
+  )
+  const ll = await db.all(
+    'SELECT * FROM auctions LIMIT 5000'
+  )
+  var t = removeDuplicates(ll, "auctionUniqueId")
+  //console.log(row)
+  //console.log(offers)
+  return res.render("auctions/auc", { data: row, offers: offers, ll: t })
+})
+
+export const auctions = router
---
+++ app/routes/categories.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response } from 'express'
+import { database } from '../database'
+const router = Router()
+
+router.get('/categories', async (req: Request, res: Response) => {
+  const db = await database()
+  const query = await db.all('SELECT DISTINCT category FROM auctions')
+  var a = query.map((row) => row['category'])
+  return res.render("categories/cat", { data: a })
+})
+
+router.get('/categories/:category', async (req: Request, res: Response) => {
+  const { category } = req.params
+  const db = await database()
+  const query = await db.all(
+    'SELECT DISTINCT subcategory FROM auctions WHERE category = ?',
+    category
+  )
+  var a = query.map((row) => row['subcategory'])
+  return res.render("categories/subcat", { data: a })
+})
+
+export const categories = router
