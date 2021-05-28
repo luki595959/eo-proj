178a114ff6e1198c355a9d6540aacf56fac809d3
luki595959 <57019378+luki595959@users.noreply.github.com>
2021-05-28 16:47:13 +0200

    Add files via upload

--- app/routes/participants.ts
+++ app/routes/participants.ts
@@ -1,16 +1,16 @@
 import { Router, Request, Response } from 'express'
 import { database } from '../database'
 const router = Router()
-
+/*
 router.get('/participants', async (req: Request, res: Response) => {
   const db = await database()
   const row = await db.all(
     'SELECT DISTINCT participant  FROM offers ORDER BY participant DESC'
   )
   var a = row.map((row) => row['participant'])
-  return res.render("participants/part", { data: a })
+  return res.render("participants/part", { data:a})
 })
-
+*/
 router.get('/participants/:id', async (req: Request, res: Response) => {
   const { id } = req.params
   const db = await database()
@@ -18,9 +18,16 @@ router.get('/participants/:id', async (req: Request, res: Response) => {
     'SELECT * FROM auctions WHERE auctionUniqueId IN (SELECT DISTINCT auctionUniqueId FROM "offers" WHERE participant = ?)',
     [id]
   )
+  const row = await db.all(
+    'SELECT DISTINCT participant  FROM offers ORDER BY participant DESC'
+  )
+  var a = row.map((row) => row['participant'])
+
+
   return res.render("participants/part_idx", {
     participant: id,
     auctions: myAuctions,
+    data: a
   })
 })
 
