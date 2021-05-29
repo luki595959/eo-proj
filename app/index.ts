77b7c67f768e783aea7aa4f8d882e4a6fa00dea1
luki595959 <57019378+luki595959@users.noreply.github.com>
2021-05-30 01:11:58 +0200

    Add files via upload

--- app/index.ts
+++ app/index.ts
@@ -4,11 +4,11 @@ import * as path from 'path';
 import { Application } from 'express'
 import * as cors from 'cors'
 import {
-  auctions,
-  global,
-  categories,
-  organizations,
-  participants,
+    auctions,
+    global,
+    categories,
+    organizations,
+    participants,
 } from './routes'
 
 const app: Application = express()
@@ -17,17 +17,15 @@ app.use(cors())
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(__dirname + '/public'));
-app.get('/aa', async (req: Request, res: Response) => {
-  return res.render("landing")
-})
 
 app.use(global)
 app.use(categories)
 app.use(auctions)
 app.use(organizations)
 app.use(participants)
+
 var PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-  console.log("The Server Has Started on Port 8080!")
+    console.log("The Server Has Started on Port 8080!")
 })
 
--- app/routes/organizations.ts
+++ app/routes/organizations.ts
@@ -4,31 +4,31 @@ const router = Router()
 
 /*
 router.get('/organizations', async (req: Request, res: Response) => {
-  const db = await database()
-  const row = await db.all(
-    'SELECT DISTINCT organization FROM auctions ORDER BY organization ASC'
-  )
-  var a = row.map((row) => row['organization'])
-  return res.render("organisations/org", { data: a })
+    const db = await database()
+    const row = await db.all(
+        'SELECT DISTINCT organization FROM auctions ORDER BY organization ASC'
+    )
+    var a = row.map((row) => row['organization'])
+    return res.render("organisations/org", { data: a })
 })
 */
 
 router.get('/organizations/:orgId', async (req: Request, res: Response) => {
-  const { orgId } = req.params
-  const db = await database()
-  const row = await db.all(
-    'SELECT DISTINCT organization FROM auctions ORDER BY organization ASC'
-  )
-  var a = row.map((row) => row['organization'])
-  const myAuctions = await db.all(
-    'SELECT * FROM auctions WHERE organization = ?',
-    [orgId]
-  )
-  return res.render("organisations/org_idx", {
-    organization: orgId,
-    auctions: myAuctions,
-    data: a
-  })
+    const { orgId } = req.params
+    const db = await database()
+    const row = await db.all(
+        'SELECT DISTINCT organization FROM auctions ORDER BY organization ASC'
+    )
+    var a = row.map((row) => row['organization'])
+    const myAuctions = await db.all(
+        'SELECT * FROM auctions WHERE organization = ?',
+        [orgId]
+    )
+    return res.render("organisations/org_idx", {
+        organization: orgId,
+        auctions: myAuctions,
+        data: a
+    })
 })
 
 export const organizations = router
